3db5fa8cf4eef4c4766829fa69146c2afc61a4c7
Irw4ntoWjy <irwantowijaya0506@gmail.com>
2025-10-28 00:16:08 +0700

    feat: updating next js version

---
+++ proxy.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function proxy(req: NextRequest) {
+	const token = req.cookies.get('token')?.value;
+	const isLoginPage = req.nextUrl.pathname.startsWith('/login');
+
+	// redirect when user not login
+	if (!token && !isLoginPage) {
+		const loginUrl = new URL('/login', req.url);
+		return NextResponse.redirect(loginUrl);
+	}
+
+	if (token && isLoginPage) {
+		const homeUrl = new URL('/technical', req.url);
+		return NextResponse.redirect(homeUrl);
+	}
+
+	return NextResponse.next();
+}
+
+export const config = {
+	matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
